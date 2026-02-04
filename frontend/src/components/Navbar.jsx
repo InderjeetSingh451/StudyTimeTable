6cb0d164c108c8d37790f50bc5f3649495e8771f
Inderjeet <itsforourfuture542@gmail.com>
2026-02-04 23:46:20 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,8 +12,12 @@ export default function Navbar() {
         to="/"
         className="px-6 py-2 text-2xl font-extrabold tracking-tight flex items-center"
       >
-        <span className="text-blue-700">Study</span>
-        <span className="text-green-600 ml-1">Planner</span>
+        <span className="bg-gradient-to-r from-indigo-700 to-purple-700 bg-clip-text text-transparent">
+          Study
+        </span>
+        <span className="bg-gradient-to-r from-cyan-500 to-teal-500 bg-clip-text text-transparent ml-1">
+          Planner
+        </span>
       </Link>
 
       {user && (
@@ -22,12 +26,10 @@ export default function Navbar() {
           <div className="w-12 h-12 rounded-full bg-black text-white flex items-center justify-center text-xl font-bold uppercase">
             {user.name?.[0]}
           </div>
-
-          {/* Logout */}
           <button
             onClick={logout}
-            className="border-2 border-black px-4 py-2 text-sm font-medium
-                       hover:bg-red-600 hover:text-white transition bg-red-400"
+            className="border border-red-300 px-4 py-2 text-sm font-medium text-red-600
+             hover:bg-red-600 hover:text-white transition rounded-md"
           >
             Logout
           </button>
@@ -36,4 +38,3 @@ export default function Navbar() {
     </nav>
   );
 }
-
